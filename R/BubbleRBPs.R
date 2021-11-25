8cf6183f0c3fbf6f10323e3f76ffb9ea4e7bf02e
Baptiste Serge Jean-Marie Kerouanton <e0545037@nus.edu.sg>
2021-11-26 06:37:10 +0800

    Commit 26/11

--- R/BubbleRBPs.R
+++ R/BubbleRBPs.R
@@ -2,17 +2,17 @@
 #'
 #' @description Use clusters from DeltaTE output to generate enrichement plot of RBPs
 #'
-#' @param symbol RBP_res, clusters
+#' @param symbol RBP_res, clusters, rbp_lfc
 #'
 #' @return NULL. Generates a bubble plot
 #'
-#' @examples BubbleRBPs(RBP_res=res,clusters=clusters)
+#' @examples BubbleRBPs(RBP_res=res,clusters=clusters,rbp_lfc=rbp_lfc)
 #'
 #' @export
 #'
 #'
 #'
-BubbleRBPs <-function(RBP_res=res,clusters=clusters)
+BubbleRBPs <-function(res=res,clusters=clusters,rbp_lfc=rbp_lfc)
 {
   #To ignore the warnings during usage
   options(warn=-1)
@@ -25,46 +25,49 @@ BubbleRBPs <-function(RBP_res=res,clusters=clusters)
     return(as.numeric(table(a%in%b)[2]*100/A))
   }
 
-  for (c in names(res)) {
-    res[[c]]=res[[c]][res[[c]]$pval<0.05,]
+  sig_res=res
+  for (cl in names(res)) {
+    sig_res[[cl]]=sig_res[[cl]][sig_res[[cl]]$pval<0.05,]
   }
 
 
   # Set up the vectors
-  RNA <- c("Trancription up","Transcription down","Transcription unchanged")
+  RNA <- c("Trancription up","Transcription\ndown","Transcription\nunchanged")
   RIBO <- c("TE unchanged","TE down","TE up")
 
   # Create the data frame
   df <- expand.grid(RNA, RIBO)
-  df$value=c(mean(res$forwarded_down$z),
-             mean(res$forwarded_up$z),
+  df$value=c(mean(sig_res$forwarded_up$z),
+             mean(sig_res$forwarded_down$z),
              NA,
-             mean(res$intensified_down$z),
-             mean(res$buffered_down$z),
-             mean(res$exclusive_down$z),
-             mean(res$buffered_up$z),
-             mean(res$intensified_up$z),
-             mean(res$exclusive_up$z))
+             mean(sig_res$buffered_down$z),
+             mean(sig_res$intensified_down$z),
+             mean(sig_res$exclusive_down$z),
+             mean(sig_res$intensified_up$z),
+             mean(sig_res$buffered_up$z),
+             mean(sig_res$exclusive_up$z))
+  df$value=round(df$value,1)
 
-  df$Regulators=c(perc_in(clusters$IDENTIFIER[clusters$Cluster=="forwarded_down"],RBP_res[[1]][,1]),
-                    perc_in(clusters$IDENTIFIER[clusters$Cluster=="forwarded_up"],RBP_res[[1]][,1]),
-                    NA,
-                    perc_in(clusters$IDENTIFIER[clusters$Cluster=="buffered_down"],RBP_res[[1]][,1]),
-                    perc_in(clusters$IDENTIFIER[clusters$Cluster=="intensified_down"],RBP_res[[1]][,1]),
-                    perc_in(clusters$IDENTIFIER[clusters$Cluster=="exclusive_down"],RBP_res[[1]][,1]),
-                    perc_in(clusters$IDENTIFIER[clusters$Cluster=="intensified_up"],RBP_res[[1]][,1]),
-                    perc_in(clusters$IDENTIFIER[clusters$Cluster=="buffered_up"],RBP_res[[1]][,1]),
-                    perc_in(clusters$IDENTIFIER[clusters$Cluster=="exclusive_up"],RBP_res[[1]][,1]))
+  df$Regulators=c(perc_in(clusters$geneID[clusters$Cluster=="forwarded_up"],rbp_lfc$geneID),
+                  perc_in(clusters$geneID[clusters$Cluster=="forwarded_down"],rbp_lfc$geneID),
+                  NA,
+                  perc_in(clusters$geneID[clusters$Cluster=="buffered_down"],rbp_lfc$geneID),
+                  perc_in(clusters$geneID[clusters$Cluster=="intensified_down"],rbp_lfc$geneID),
+                  perc_in(clusters$geneID[clusters$Cluster=="exclusive_down"],rbp_lfc$geneID),
+                  perc_in(clusters$geneID[clusters$Cluster=="intensified_up"],rbp_lfc$geneID),
+                  perc_in(clusters$geneID[clusters$Cluster=="buffered_up"],rbp_lfc$geneID),
+                  perc_in(clusters$geneID[clusters$Cluster=="exclusive_up"],rbp_lfc$geneID))
 
   df$Regulators[is.na(df$Regulators)]=0
-  df$label=c("forwarded_down","forwarded_up",NA,"buffered_down","intensified_down",
+  df$label=c("forwarded_up","forwarded_down",NA,"buffered_down","intensified_down",
              "exclusive_down","intensified_up","buffered_up","exclusive_up")
 
   #Plot the Data
-  ggplot(df, aes(Var1, Var2,label=label)) + geom_point(aes(size = value, colour = Regulators)) +
-    scale_size_continuous(range = c(4, 8))+
+  ggplot(df, aes(Var1, Var2,label=value)) + geom_point(aes(size = value, colour = Regulators)) +
+    scale_size_continuous(range = c(1,10))+
     scale_colour_gradient(low = "white", high = "darkblue")+
-    theme_bw() + xlab("") + ylab("")+  geom_text(hjust=0.5, vjust=-3)+
+    theme_bw() + xlab("") + ylab("")+  geom_text(hjust=0.5, vjust=-2)+
+    ggtitle("Mean z-score per gene group")+
     theme(panel.border = element_blank(), panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+
     guides(col=guide_legend(title="Percentage of RBP"),
--- R/HeatmapRBP.R
+++ R/HeatmapRBP.R
@@ -2,7 +2,7 @@
 #'
 #' @description Plot a heatmap of the pvalues of enrichment of each RBP in each cluster
 #'
-#' @param symbol res, rbp_lfc, grid
+#' @param symbol res, rbp_lfc
 #'
 #' @return Plot a heatmap
 #'
@@ -12,37 +12,47 @@
 #'
 #'
 #'
-HeatmapRBP <-function(res=res,rbp_lfc=rbp_lfc,grid=F)
+HeatmapRBP <-function(res=res,rbp_lfc=rbp_lfc)
 {
   #To ignore the warnings during usage
   options(warn=-1)
   options("getSymbols.warning4.0"=FALSE)
   options(stringsAsFactors=FALSE);
 
-  pvalues=data.frame(value1=res[[1]]$pval)
+  pvalues = data.frame(value1 = res[[1]]$pval)
+
   for (i in names(res)[-1]) {
-    pvalues=cbind(pvalues,res[[i]]$pval)
-  }
-  pvalues=-log10(pvalues)
-  pvalues[which(pvalues==Inf,arr.ind = T)]=5
-  rownames(pvalues)=res[[1]]$RBP
-  colnames(pvalues)=names(res)
-  pvalues=pvalues[rowSums(pvalues>-log10(0.05))>0,]
-
-  pvalues$status=NA
-  pvalues$status[match(rbp_lfc$IDENTIFIER,rownames(pvalues))]=sign(rbp_lfc$FoldChange)
-  pvalues$colors=ifelse(pvalues$status==1,"dodgerblue3","darkorange1")
-  pvalues=pvalues[!is.na(pvalues$colors),]
-
-  if (grid==T) {
-    grid="grey"
+    pvalues = cbind(pvalues, res[[i]]$pval)
   }
 
-  e=pheatmap(pvalues[,1:(ncol(pvalues)-2)],breaks =0:5,color = colorRampPalette(c("oldlace","darkred"))(5),
-             angle_col = 45,legend = T,border_color = grid,main="-log(P) of RBP's targets enrichment per cluster")#,fontsize_row = 6)
-  cols=pvalues[order(match(rownames(pvalues), e$gtable$grobs[[6]]$label)), ]$colors
-  e$gtable$grobs[[6]]$gp=gpar(col=cols)
-  return(e)
+  pvalues = -log10(pvalues)
+  pvalues[which(pvalues == Inf, arr.ind = T)] = 5
+  rownames(pvalues) = res[[1]]$RBP
+  colnames(pvalues) = names(res)
+  pvalues = pvalues[rowSums(pvalues > -log10(0.05)) > 0, ]
+  pvalues=trunc(pvalues)
+  pvalues$status = NA
+  pvalues$status[match(rbp_lfc$IDENTIFIER, rownames(pvalues))] = sign(rbp_lfc$FoldChange)
+  pvalues$colors = ifelse(pvalues$status == 1, "dodgerblue3",
+                          "darkorange1")
+  pvalues = pvalues[!is.na(pvalues$colors), ]
+  colnames(pvalues)=sub("_"," ",colnames(pvalues))
+
+  colorRBP=as.character(pvalues$status)
+  names(colorRBP)=rownames(pvalues)
+
+  HT1=Heatmap(pvalues[,-c(9,10)],column_title = "RBP enrichment per gene group",
+              col = colorRampPalette(c("oldlace", "darkred"))(5),name = "-log(P)",
+              column_names_rot=45,show_row_names = T,width = unit(10,"cm"),
+              heatmap_legend_param =list(at = 0:5))
+
+  HT2=Heatmap(colorRBP,show_column_names=F,
+              show_row_names = T,name = "RBP direction",
+              col=c("dodgerblue3","darkorange1"),width = unit(0.5,"cm"),
+              heatmap_legend_param = list(at=c("-1","1"),labels=c("Down","Up")))
+
+  A=HT1+HT2
+  return(A)
 
 }
 
