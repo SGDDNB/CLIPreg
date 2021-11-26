d663f548968b0ca462bafad54b9b014ad723c829
Baptiste Serge Jean-Marie Kerouanton <e0545037@nus.edu.sg>
2021-11-26 15:49:33 +0800

    Commit 26/11

--- R/Visualise.R
+++ R/Visualise.R
@@ -27,20 +27,20 @@ Visualise=function(results=results,folder=getwd()){
   dev.off()
 
   C=Draw_network_by_group(rbp_lfc=rbp_lfc,res=res,Targets=Targets,gene_groups=gene_groups,n=5,forwarded = F)
-  pdf(paste0(folder,"/Network_5_most_DE_RBP.pdf"),6,4.5)
+  pdf(paste0(folder,"/Network_5_most_DE_RBP.pdf"),6,4.5,useDingbats = F)
   print(C)
   dev.off()
 
   D=Plot_GO(rbp_lfc=rbp_lfc,res=res,Targets=Targets,gene_groups=gene_groups,n=5,
           tpm_ribo = tpm_ribo,th=200,GO_to_show=3,forwarded=F)
   n=nrow(D$data)
-  pdf(paste0(folder,"/GO_from_nodes_200+.pdf"),12,3+0.3*n)
+  pdf(paste0(folder,"/GO_from_nodes_200+.pdf"),12,3+0.3*n,useDingbats = F)
   print(D)
   dev.off()
 
   rbp_of_interest=as.character(rbp_lfc$IDENTIFIER)[1]
   E=Plot_GO_RBP(rbp_of_interest=rbp_of_interest,tpm_ribo = tpm_ribo,Targets=Targets,gene_groups=gene_groups,GO_to_show=3)
-  pdf(paste0(folder,"/GO_RBP_",rbp_of_interest,".pdf"),12,3+n*0.3)
+  pdf(paste0(folder,"/GO_RBP_",rbp_of_interest,".pdf"),12,3+n*0.3,useDingbats = F)
   print(E)
   dev.off()
 
--- R/run_CLIPreg.R
+++ R/run_CLIPreg.R
@@ -14,8 +14,8 @@ run_CLIPreg=function(input_data=Example, is.example=T)
   RBP_POSTAR=input_data$RBP_POSTAR
 
   if (is.example==T) {
-    res_Encode=CLIPreg::res_Encode
-    res_Postar=CLIPreg::res_Postar
+    data("Res_Encode")
+    data("Res_Postar")
   } else {
     res_Encode=CLIPreg(RBP_data=RBP_ENCODE,gene_groups=gene_groups)
     res_Postar=CLIPreg(RBP_data=RBP_POSTAR,gene_groups=gene_groups)
