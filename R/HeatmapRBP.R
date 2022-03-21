53855488320b9c0af71bed3858e3884c84bc5876
baptiste-ker <79191160+baptiste-ker@users.noreply.github.com>
2022-03-21 11:09:42 +0800

    Commit 21/03/2021

--- R/HeatmapRBP.R
+++ R/HeatmapRBP.R
@@ -33,6 +33,7 @@ HeatmapRBP <-function(res=res,rbp_lfc=rbp_lfc)
   colnames(pvalues) = names(res)
   pvalues = pvalues[rowSums(pvalues > -log10(0.05)) > 0, ]
   pvalues=round(pvalues)
+  rbp_lfc=rbp_lfc[rbp_lfc$IDENTIFIER%in%rownames(pvalues),]
   pvalues$status = NA
   pvalues$status[match(rbp_lfc$IDENTIFIER, rownames(pvalues))] = sign(rbp_lfc$FoldChange)
   pvalues$colors = ifelse(pvalues$status == 1, "dodgerblue3",
@@ -44,13 +45,13 @@ HeatmapRBP <-function(res=res,rbp_lfc=rbp_lfc)
   names(colorRBP)=rownames(pvalues)
 
   HT1=Heatmap(pvalues[,-c(ncol(pvalues)-1,ncol(pvalues))],column_title = "RBP enrichment per gene group",
-              col = colorRampPalette(c("oldlace", "darkred"))(max(pvalues)),name = "-log(FDR)",
+              col = colorRampPalette(c("oldlace", "darkred"))(max(pvalues[,1:(ncol(pvalues)-2)])),name = "-log(FDR)",
               column_names_rot=45,show_row_names = T,width = unit(ncol(pvalues),"cm"),
-              heatmap_legend_param =list(at = 0:max(pvalues)))
+              heatmap_legend_param =list(at = 0:max(pvalues[,1:(ncol(pvalues)-2)])))
 
   HT2=Heatmap(colorRBP,show_column_names=F,
               show_row_names = T,name = "RBP direction",
-              #col=c("dodgerblue3","darkorange1"),width = unit(0.5,"cm"),
+              col=c("dodgerblue3","darkorange1"),width = unit(0.5,"cm"),
               heatmap_legend_param = list(at=c("-1","1"),labels=c("Down","Up")))
 
   A=HT1+HT2
